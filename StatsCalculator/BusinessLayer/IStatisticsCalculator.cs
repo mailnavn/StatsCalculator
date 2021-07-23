0a80d24fd18811f874768194e3e58fd5c61bc30a
Naveen K <mailnavn@gmail.com>
2021-07-24 00:08:49 +0100

    Adding interface for calculating arithmetic mean, SD, Frequencies of Numbers

---
+++ StatsCalculator/BusinessLayer/IStatisticsCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatsCalculator.BusinessLayer
+{
+    /// <summary>
+    /// The interface contains various methods for calculating statistical data
+    /// </summary>
+    public interface IStatisticsCalculator
+    {
+
+        double CalculateArithmeticMean(List<double> values);
+
+        double CalculateStandardDeviation(List<double> values);
+
+        double FrequencyOfNumbersInBinTen(List<double> values);
+
+
+    }
+}
